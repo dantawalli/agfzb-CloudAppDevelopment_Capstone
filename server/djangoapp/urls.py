c2e2b5f6eadc0deb0e1a342d8b56da7f129e2725
dantawalli <buharinasir1996@gmail.com>
2023-06-10 13:32:30 -0400

    some changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
      path('', views.index, name='index'),
      path('about', views.about, name='about'),
      path('contact', views.contact, name='contact'),
+     path('login/', views.login_view, name='login'),
+     path('logout/', views.logout_view, name='logout'),
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -22,17 +22,24 @@ def about(request):
 def contact(request):
     return render(request, 'djangoapp/contact.html')
 
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'djangoapp/login.html', {'error': 'Invalid username or password.'})
+    else:
+        return render(request, 'djangoapp/login.html')
 
-# Create a `contact` view to return a static contact page
-#def contact(request):
 
-# Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def logout_view(request):
+    logout(request)
+    return redirect('djangoapp:index')
 
-# Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
